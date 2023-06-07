a977c180cc142c0e8ffa81684326a6352c308b1e
ryanala <2970616108@qq.com>
2023-06-07 08:08:26 +0800

    爬楼梯(动态数组)

---
+++ 爬楼梯(动态数组)/爬楼梯(动态数组).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Climb
+{
+private:
+	int n;
+public:
+	void fun(int a) { n = a; };
+	int climb(int n)
+	{
+		int* pa = new int[n + 1];
+	}
+};
+int main()
+{
+	int n;
+	cin >> n;
+
+	
+}
+
+int function(int n)
+{
+	Climb c;
+	c.fun(n);
+	c.climb(n);
+}
