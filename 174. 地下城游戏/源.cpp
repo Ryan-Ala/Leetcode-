dfb61ce3526fffb4162c11cb3450d7da9fb3230e
ryanala <2970616108@qq.com>
2023-09-08 08:03:13 +0800

    174. 地下城游戏

---
+++ 174. 地下城游戏/源.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+        dp[m - 1][n] = dp[m][n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--)
+            for (int j = n - 1; j >= 0; j--)
+            {
+                dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+                if (dp[i][j] <= 0)
+                    dp[i][j] = 1;
+            }
+        return dp[0][0];
+    }
+};
