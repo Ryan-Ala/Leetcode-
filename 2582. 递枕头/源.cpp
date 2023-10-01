5fd4b9f5e967236a79fbdd5ff513975b82454dcf
ryanala <2970616108@qq.com>
2023-10-01 09:30:49 +0800

    2582. 递枕头

---
+++ 2582. 递枕头/源.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if ((time / (n - 1)) % 2)
+        {
+            return n - (time % (n - 1));
+        }
+        else
+        {
+            return time % (n - 1) + 1;
+        }
+    }
+};
