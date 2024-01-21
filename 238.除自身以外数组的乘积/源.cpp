302fb698beb47a3791bbba8ca4861b6a848da54e
ryanala <2970616108@qq.com>
2024-01-21 19:12:12 +0800

    238.除自身以外数组的乘积

---
+++ 238.除自身以外数组的乘积/源.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> f(n, 1), g(n, 1), ans(n);
+        for (int i = 1; i < n; i++)
+            f[i] = f[i - 1] * nums[i - 1];
+        for (int i = n - 2; i > -1; i--)
+            g[i] = g[i + 1] * nums[i + 1];
+        for (int i = 0; i < n; i++)
+            ans[i] = f[i] * g[i];
+        return ans;
+    }
+};
