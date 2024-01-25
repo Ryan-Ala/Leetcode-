63eb020e8d9eca0c94519216ba568eb1f9212c8b
ryanala <2970616108@qq.com>
2024-01-25 09:41:34 +0800

    面试题 17.06. 2出现的次数

---
+++ 面试题 17.06. 2出现的次数/源.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOf2sInRange(int n) {
+        int count = 0, num = n;
+        long long i = 1;
+        while (num)
+        {
+            if (num % 10 < 2)
+                count += num / 10 * i;
+            else if (num % 10 == 2)
+                count += num / 10 * i + (n % i) + 1;
+            else
+                count += ceil(num / 10.0) * i;
+            i *= 10;
+            num /= 10;
+        }
+        return count;
+    }
+};
