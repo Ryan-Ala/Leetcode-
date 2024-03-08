c189a4f7005a6405cef18981d5854bd59e0c0c8d
ryanala <2970616108@qq.com>
2024-03-08 11:46:07 +0800

    2917. 找出数组中的 K-or 值

---
+++ 2917. 找出数组中的 K-or 值/源.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findKOr(vector<int>& nums, int k) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            int cnt = 0;
+            for (auto e : nums)
+            {
+                cnt += (e >> i & 1);
+            }
+            if (cnt >= k)
+            {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+
+    }
+};
