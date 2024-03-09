05cccf317db4b8c3c13d0e265798adc3e31b2b82
ryanala <2970616108@qq.com>
2024-03-10 07:54:33 +0800

    1046. 最后一块石头的重量

---
+++ 1046. 最后一块石头的重量/源.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> q;
+        for (auto& e : stones)
+            q.push(e);
+
+        while (q.size() > 1)
+        {
+            int a = q.top(); q.pop();
+            int b = q.top(); q.pop();
+            if (a != b)
+                q.push(abs(a - b));
+        }
+        if (q.size() == 1)
+            return q.top();
+        return 0;
+    }
+};
