4e8e90542932bfe2bc5d4e2c77cfc08c9cbe5a2a
ryanala <2970616108@qq.com>
2024-08-08 13:19:07 +0800

    226.翻转二叉树

---
+++ 226.翻转二叉树/源.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root)
+{
+    if (!root)
+        return root;
+    struct TreeNode* p = root->left;
+    root->left = root->right;
+    root->right = p;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
